b1515cfb6aff7889a36c97e7389f9d3aa139d1f1
Vriendelik <ntebohelengd@gmail.com>
2023-11-10 07:38:17 -0800

    Betty would be proud

---
+++ README.md
@@ -0,0 +1,1 @@
+My Readme file for simple shell project that i am doing solo
---
+++ triangle.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - A function that prints a triangle
+ * Return: 0 on success
+ */
+
+int main(void)
+
+{
+	printf("     *\n");
+	printf("    * *\n");
+	printf("   *   *\n");
+	printf("  *     *\n");
+	printf(" *       *\n");
+	printf("* * * * * *\n");
+
+return (0);
+}
